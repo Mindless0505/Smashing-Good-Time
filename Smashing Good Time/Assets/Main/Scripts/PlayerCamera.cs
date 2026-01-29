c93165acf36e67220f89bfb869c3c45440597ef2
gavinmpierce <157523429+gavinmpierce@users.noreply.github.com>
2026-01-28 22:58:04 -0500

    Start of Rigid Body Movement

---
+++ Smashing Good Time/Assets/Main/Scripts/PlayerCamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    public float xRotation;
+    public float yRotation;
+
+    void Start()
+    {
+        // lock cursor in middle and stop it from being visible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get the mouse input
+        float mouseX = input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensX;
+
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        xRotation = Math.Clamp(xRotation,-90f,90f);
+
+        // rotate cam and orientation
+        transform.rotation = Quaternion.Euler(xRotation,yRotation,0);
+        orientation.rotation = Quaternion.Euler(0,yRotation,0);
+        
+    }
+}
