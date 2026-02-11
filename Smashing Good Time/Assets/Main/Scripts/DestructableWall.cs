b6bd404fdea365ae6083d96134068c81121a9f3b
gavinmpierce <gavin.m.pierce@maine.edu>
2026-02-10 19:14:23 -0500

    Working wall destruction

--- Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
+++ Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class DestructableWall : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Impact Settings")]
+    // set the required impact strength and health threshold for the wall to be destroyed
+    public float requiredImpact = 5f;
+    public float requiredHealth = 50f;
+    [SerializeField] private Transform PrefabWall;
+    
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+        // calculate the impact strength based on the relative velocity of the collision
+        float impactStrength = collision.relativeVelocity.magnitude;
+        requiredHealth -= impactStrength;
+        Debug.Log("Impact strength: " + impactStrength);
+        // check if the impact strength meets the required threshold or if the health is depleted
+        if (impactStrength >= requiredImpact || requiredHealth <= 0)
+        {
+            SwitchObject();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SwitchObject()
     {
-        
+        // Instantiate the new wall prefab at the same position and rotation as the original wall
+        Instantiate(PrefabWall, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Debug.Log("Wall destroyed");
     }
+    
 }
