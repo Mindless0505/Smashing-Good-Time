f99985856b964e6db10cde8c3df24f3d94c31ecc
Mindless0505 <meep514@gmail.com>
2026-02-18 16:04:43 -0500

    nvm

---
+++ Smashing Good Time/Assets/Main/Scripts/Movement/PlayerMovement.cs
@@ -0,0 +1,146 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float originalMoveSpeed;
+    public float sprintSpeed;
+    public float crouchSpeed;
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    public bool jumpReady;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+    public KeyCode crouchKey = KeyCode.LeftControl;
+
+    [Header("Ground Check")]
+    
+    public float playerHeight;
+    public LayerMask WhatIsGround;
+    public bool grounded;
+
+    
+
+    public Transform orientation;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!IsOwner)
+        {
+            return;
+        }
+
+        // ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, WhatIsGround);
+
+        MyInput();
+        SpeedControl();
+        // handle drag
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+    }
+    private void FixedUpdate() 
+    {
+        MovePlayer();
+    }
+
+    private void MyInput() 
+    {
+        // Default speed
+        moveSpeed = originalMoveSpeed;
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // jump
+        if (Input.GetKey(jumpKey) && jumpReady && grounded) 
+        {
+            jumpReady = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+        
+
+        // Sprint
+        if (Input.GetKey(sprintKey))
+        {
+            Sprint();
+        }
+
+        // Crouch (overrides sprint if both pressed)
+        if (Input.GetKey(crouchKey))
+        {
+            Crouch();
+        }
+
+    }
+
+    private void MovePlayer() 
+    {
+        // calculate movement direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        // on ground
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        // in air
+        else
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+    }
+
+    private void Jump() 
+    {
+        // reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void Sprint()
+    {
+        moveSpeed = sprintSpeed;
+    }
+
+    private void Crouch()
+    {
+        moveSpeed = crouchSpeed;
+    }
+    private void ResetJump() 
+    {
+        jumpReady = true;
+    }
+
+    private void SpeedControl() 
+    {         
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed) 
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+}
