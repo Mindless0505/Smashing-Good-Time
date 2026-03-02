262c3baceb5f509125528b8968664ed21b7247e8
gavinmpierce <gavin.m.pierce@maine.edu>
2026-03-01 22:50:09 -0500

    network additions

--- Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
+++ Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
@@ -1,6 +1,7 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class DestructableWall : MonoBehaviour
+public class DestructableWall : NetworkBehaviour
 {
     [Header("Impact Settings")]
     // set the required impact strength and health threshold for the wall to be destroyed
@@ -10,28 +11,62 @@ public class DestructableWall : MonoBehaviour
     
     public bool isDestroyed = false;
 
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    Debug.Log("Collision detected with " + collision.gameObject.name);
+
+    //    // calculate the impact strength based on the relative velocity of the collision
+    //    float impactStrength = collision.relativeVelocity.magnitude;
+    //    requiredHealth -= impactStrength;
+    //    Debug.Log("Impact strength: " + impactStrength);
+
+    //    // check if the impact strength meets the required threshold or if the health is depleted
+    //    if (impactStrength >= requiredImpact || requiredHealth <= 0)
+    //    {
+    //        DestroyWall();
+    //    }
+    //}
     void OnCollisionEnter(Collision collision)
     {
-        if (isDestroyed) return; // prevent multiple destructions
+        if (!IsServer) return;
+
         Debug.Log("Collision detected with " + collision.gameObject.name);
+
         // calculate the impact strength based on the relative velocity of the collision
         float impactStrength = collision.relativeVelocity.magnitude;
         requiredHealth -= impactStrength;
         Debug.Log("Impact strength: " + impactStrength);
+
         // check if the impact strength meets the required threshold or if the health is depleted
         if (impactStrength >= requiredImpact || requiredHealth <= 0)
         {
-            isDestroyed = true;
-            SwitchObject();
+            DestroyWall();
         }
     }
 
     void SwitchObject()
     {
         // Instantiate the new wall prefab at the same position and rotation as the original wall
-        Instantiate(PrefabWall, transform.position, transform.rotation);
+        Transform wall = Instantiate(PrefabWall, transform.position, transform.rotation);
+        
+        NetworkObject networkObject = wall.GetComponent<NetworkObject>();
+        networkObject.Spawn();
         Destroy(gameObject);
         Debug.Log("Wall destroyed");
     }
-    
+
+    void DestroyWall()
+    {
+        if (isDestroyed) return;
+
+        isDestroyed = true;
+
+        Transform wall = Instantiate(PrefabWall, transform.position, transform.rotation);
+        wall.GetComponent<NetworkObject>().Spawn(true);
+
+        GetComponent<NetworkObject>().Despawn(true);
+
+        Debug.Log("Wall destroyed");
+    }
+
 }
