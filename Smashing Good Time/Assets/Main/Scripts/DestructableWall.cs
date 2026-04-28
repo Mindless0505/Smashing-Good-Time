517955bf60a7703a8a3f92156e80c901efe06d63
Mindless0505 <meep514@gmail.com>
2026-04-28 12:07:05 -0400

    changes

--- Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
+++ Smashing Good Time/Assets/Main/Scripts/DestructableWall.cs
@@ -91,20 +91,20 @@ public class DestructableWall : NetworkBehaviour
         }
         else
         {
-            // if (impactStrength >= requiredFallImpact)
-            // {
-            //     Wallrb.constraints = RigidbodyConstraints.None;
-            //     // netObj.enabled = true;
-            //     netRb.enabled = true;
-            //     netTransform.enabled = true;
+            if (impactStrength >= requiredFallImpact)
+            {
+                Wallrb.constraints = RigidbodyConstraints.None;
+                // netObj.enabled = true;
+                netRb.enabled = true;
+                netTransform.enabled = true;
 
-            //     Vector3 impulse = collision.impulse;
-            //     Wallrb.AddForce(impulse, ForceMode.Impulse);
-            // }
+                Vector3 impulse = collision.impulse;
+                Wallrb.AddForce(impulse, ForceMode.Impulse);
+            }
 
             // Client reports collision to server
-            SharedPhysics sp = GetComponent<SharedPhysics>();
-            if (sp != null)
+            // SharedPhysics sp = GetComponent<SharedPhysics>();
+            // if (sp != null)
                 ReportImpactServerRpc(impactStrength);
         }
     }
